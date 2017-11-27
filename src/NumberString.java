ce839d924e767b387be62cb2a1556e52081eb29c
Samantha Rutherford <33076449+srutherford1@users.noreply.github.com>
2017-11-26 21:04:19 -0500

    Number String
    The pseudocode which I suck at and the code itself. I think I could have done another nested for loop but I was confused as to which of the two inner ones would come first.

--- src/NumberString.java
+++ src/NumberString.java
@@ -1,10 +1,21 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Samantha Rutherford
+  	Date: 11/26/2017
 */
 
-/* 6. Your pseudocode algorithm here
+/* 6.
+ * Create a for loop that will
+ *
+ * 	have an int i = 1 which loop another loop 3 times
+ *
+ * 	in the nested loop an int j = 1 which will loop 10 times.
+ *
+ * 		in the loop you will print j 10 times.
+ *
+ * 		You will do this line three times.
+ *
+ * 	You will print these on separate lines using println.
 
 
 */
@@ -13,6 +24,15 @@ public class NumberString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		for (int i=1; i<4; i++) {
+			for (int j=1; j<10; j++) {
+
+				System.out.print(j);
+				System.out.print(j);
+				System.out.print(j);
+			}
+			System.out.println();
+		}
 
 	}
 
