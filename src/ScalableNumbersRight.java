d1ffe83ce0f98e1f63822e1d3200fe97ed3bf7a9
Samantha Rutherford <33076449+srutherford1@users.noreply.github.com>
2017-11-26 21:18:32 -0500

    ScalableNumberRight
    So i assume when you said scalable meaning that the user can choose the scale of how big or small it is right? So I did that based off my interpretation of the instructions.

--- src/ScalableNumbersRight.java
+++ src/ScalableNumbersRight.java
@@ -1,14 +1,17 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Samantha Rutherford
+  	Date: 11/26/2017
 */
-
+import java.util.Scanner;
 public class ScalableNumbersRight {
 
 	public static void main(String[] args) {
-		for( int line = 1; line <= 5; line++ ) {
-			for( int spacesIndex = 1; spacesIndex <= 5 - line; spacesIndex++ ) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Choose scale for the right number triangle thing: ");
+		int num = scan.nextInt();
+		for( int line = 1; line <= num; line++ ) {
+			for( int spacesIndex = 1; spacesIndex <= num - line; spacesIndex++ ) {
 				System.out.print(" ");
 			}
 			for( int numbersIndex = 1; numbersIndex <= line; numbersIndex++ ) {
