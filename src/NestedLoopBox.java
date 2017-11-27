4359148f42c42a18c1a35bffa0a3c5d10f262c3c
Samantha Rutherford <33076449+srutherford1@users.noreply.github.com>
2017-11-26 20:44:21 -0500

    NestedLoops
    Wrote the pseudocode which I just suck at and wrote out basic code. Are we like allowed to do more to it? I would like to use the Scanner class to make it so you can choose the size and then would like to add a do while loop so we can choose to do it again.

--- src/NestedLoopBox.java
+++ src/NestedLoopBox.java
@@ -1,10 +1,24 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Samantha Rutherford
+  	Date: 11/26/2017
 */
 
-/* 4. Your pseudocode algorithm here
+/* 4.
+ * create a for loop
+ *  that declares width as an integer to equal 0;
+ *  width is to loop less than six times to repeat the next loop
+ *  height is an integer that is equal to 0.
+ *  height is to print out six asterisks six times making:
+ *
+ *     ******
+ *     ******
+ *     ******
+ *     ******
+ *     ******
+ *     ******
+ *
+ * Program ends.
 
 
 */
@@ -12,7 +26,12 @@ public class NestedLoopBox {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+			for (int width = 0; width < 6; width++){
+				for (int height = 0; height < 6; height++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
 	}
 
 }
