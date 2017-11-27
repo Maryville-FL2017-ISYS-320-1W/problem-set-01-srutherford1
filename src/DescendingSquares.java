595f739ce74bdb5a156867741f4a20e8cfee8be5
Samantha Rutherford <33076449+srutherford1@users.noreply.github.com>
2017-11-26 20:02:04 -0500

    DescendingSquares
    I added the pseudocode and basic code to make this program function.

--- src/DescendingSquares.java
+++ src/DescendingSquares.java
@@ -1,10 +1,16 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Samantha Rutherford
+  	Date: 11/29/2017
 */
 
 /* 2. Your pseudocode algorithm here
+ * Create a for loop
+ * 	{
+ * 		declare an integer called i to be equal to 10
+ * 		have said integer to be greater than 0
+ * 		and have said integer to go backwards with i--
+ * 		After print out i to multiply by itself following a space to space out numbers.
 
 
 */
@@ -12,7 +18,9 @@
 public class DescendingSquares {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (int i = 10; i > 0; i--) {
+			System.out.print(i*i + " ");
+		}
 
 	}
 
